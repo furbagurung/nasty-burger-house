974350f71c894688a703942bb2523cc222db0d3e
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-05 02:35:41 +0545

    Use clean homepage menu categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import HomeMenuCategoriesEnhancer from "./components/home-menu-categories-enhancer";
 import OrderExperience from "./components/order-experience";
 import { menuItems } from "./data/menu";
 import { getServiceStatus } from "./lib/service";
 
 export default function Home() {
   return (
-    <OrderExperience
-      items={menuItems}
-      initialServiceStatus={getServiceStatus()}
-    />
+    <>
+      <OrderExperience
+        items={menuItems}
+        initialServiceStatus={getServiceStatus()}
+      />
+      <HomeMenuCategoriesEnhancer />
+    </>
   );
 }

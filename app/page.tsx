0e909f8da0fccd70d618a676fe7e64f4754e8d58
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-04 23:36:01 +0545

    Remove duplicate home header

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import HomeTopHeader from "./components/home-top-header";
 import OrderExperience from "./components/order-experience";
 import { menuItems } from "./data/menu";
 import { getServiceStatus } from "./lib/service";
 
 export default function Home() {
   return (
-    <>
-      <HomeTopHeader />
-      <OrderExperience
-        items={menuItems}
-        initialServiceStatus={getServiceStatus()}
-      />
-    </>
+    <OrderExperience
+      items={menuItems}
+      initialServiceStatus={getServiceStatus()}
+    />
   );
 }

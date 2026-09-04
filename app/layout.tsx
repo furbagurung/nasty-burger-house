3e9b1fccc5b75e8fb25969072f0b5b1e38db9a5a
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-04 21:32:53 +0545

    Load home header styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./catalogue-theme.css";
 import "./mobile-app.css";
+import "./home-top-header.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

f84d5ffb5d692f26a41573b884a6357d93ac84e3
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-04 21:32:45 +0545

    Use new desktop home header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import HomeTopHeader from "./components/home-top-header";
 import OrderExperience from "./components/order-experience";
 import { menuItems } from "./data/menu";
 import { getServiceStatus } from "./lib/service";
 
 export default function Home() {
   return (
-    <OrderExperience
-      items={menuItems}
-      initialServiceStatus={getServiceStatus()}
-    />
+    <>
+      <HomeTopHeader />
+      <OrderExperience
+        items={menuItems}
+        initialServiceStatus={getServiceStatus()}
+      />
+    </>
   );
 }

a8824ab65e66ed6d8d8c5af7ab5a90d86d22a054
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-06 01:38:27 +0545

    Guard coming soon home menu card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FindUsSection from "./components/find-us-section";
 import HomeBeastMonthEnhancer from "./components/home-beast-month-enhancer";
 import HomeMenuCategoriesEnhancer from "./components/home-menu-categories-enhancer";
+import HomeMenuComingSoonGuard from "./components/home-menu-coming-soon-guard";
 import OrderExperience from "./components/order-experience";
 import { menuItems } from "./data/menu";
 import { getServiceStatus } from "./lib/service";
@@ -15,6 +16,7 @@ export default function Home() {
       <FindUsSection />
       <HomeBeastMonthEnhancer />
       <HomeMenuCategoriesEnhancer />
+      <HomeMenuComingSoonGuard />
     </>
   );
 }

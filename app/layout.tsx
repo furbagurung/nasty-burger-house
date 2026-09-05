1935ccd98ef013f5861e38c9dfd2c5152c193093
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-06 01:38:57 +0545

    Load client revision polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ import "./mobile-bottom-nav-v2.css";
 import "./standalone-pages.css";
 import "./customer-account.css";
 import "./customer-header-overrides.css";
+import "./client-revision-polish.css";
 import "./admin-dashboard.css";
 import "./admin-notification-ui.css";
 

e0731e8d75bbdf25f8c919052ba7954c6552900c
Furba Gurung <30387153+furbagurung@users.noreply.github.com>
2026-09-06 18:58:11 +0545

    Remove floating dark mobile bottom nav override

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ import "./mobile-home-location.css";
 import "./admin-dashboard.css";
 import "./admin-notification-ui.css";
 import "./mobile-header-always-dark.css";
-import "./mobile-bottom-nav-floating.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
